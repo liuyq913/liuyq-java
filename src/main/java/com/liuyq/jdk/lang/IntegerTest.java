65b2370230759b80538a3a62fd8d225033fcc100
liuyqq <liuyq@cheok.com>
2018-03-01 17:01:59 +0800

    避免过度使用同步

---
+++ src/main/java/com/liuyq/jdk/lang/IntegerTest.java
@@ -0,0 +1,38 @@
+package com.liuyq.jdk.lang;
+
+import org.junit.Test;
+
+/**
+ * Created by liuyq on 2018/3/1.
+ */
+public class IntegerTest {
+
+    final static int [] sizeTable = { 9, 99, 999, 9999, 99999, 999999, 9999999,
+            99999999, 999999999, Integer.MAX_VALUE };
+
+    static int stringSize(int x) {
+        for (int i=0; ; i++)
+            if (x <= sizeTable[i])
+                return i+1;
+    }
+
+    @Test
+    public void StringTest(){
+        Integer integer = Integer.MIN_VALUE;
+        System.out.println(integer.toString(integer, 10));
+    }
+    @Test
+    public void stringSizeTest(){
+        System.out.println(stringSize(123));
+    }
+
+    @Test
+    public void charAtTest(){
+        System.out.println("123".charAt(0));
+    }
+
+    @Test
+    public void decodeTest(){
+        System.out.println(Integer.decode("-10"));
+    }
+}
