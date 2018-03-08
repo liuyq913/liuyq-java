b23fe271d899f02e648410e46447c835dcbd5209
liuyqq <liuyq@cheok.com>
2018-03-08 14:31:48 +0800

    String源码

--- src/main/java/com/liuyq/jdk/lang/StringTest.java
+++ src/main/java/com/liuyq/jdk/lang/StringTest.java
@@ -5,7 +5,45 @@ import org.junit.Test;
 /**
  * Created by liuyq on 2018/3/2.
  */
-public class StringTest {
+    public class StringTest {
+
+    static int indexOf(char[] source, int sourceOffset, int sourceCount,
+                   char[] target, int targetOffset, int targetCount,
+                   int fromIndex) {
+        if (fromIndex >= sourceCount) {
+            return (targetCount == 0 ? sourceCount : -1);
+        }
+        if (fromIndex < 0) {
+            fromIndex = 0;
+        }
+        if (targetCount == 0) {
+            return fromIndex;
+        }
+
+        char first = target[targetOffset];
+        int max = sourceOffset + (sourceCount - targetCount);
+
+        for (int i = sourceOffset + fromIndex; i <= max; i++) {
+            /* Look for first character. */
+            if (source[i] != first) {
+                while (++i <= max && source[i] != first);
+            }
+
+            /* Found first character, now look at the rest of v2 */
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + targetCount - 1;
+                for (int k = targetOffset + 1; j < end && source[j]
+                        == target[k]; j++, k++);
+
+                if (j == end) {
+                    /* Found whole string. */
+                    return i - sourceOffset;
+                }
+            }
+        }
+        return -1;
+    }
     @Test
     public void hashTest(){
         System.out.println("123".hashCode());
@@ -44,7 +82,7 @@ public class StringTest {
     }
     @Test
     public void testIndexOf(){
-        String s = "hello world";
+       /* String s = "hello world";
         //System.out.println(s.indexOf("w",4)); //当目标字符串是空串的时候，直接返回开始索引的值
        // System.out.println(s.indexOf("",-1)); //当开始索引值小于0的时候，返回0
 
@@ -55,12 +93,60 @@ public class StringTest {
                 System.out.println("这");
             }
             System.out.println(chars[i]);
-        }
+        }*/
+        char[] source = {'0','1','2','3','4','5','6','7','8'};
+        char[] target ={'2','6','4'};
+        System.out.println(indexOf(source, 0, 9, target, 0,3, 0));
+
     }
 
     @Test
     public void testConcat(){
         String s = "liu";
         System.out.println(s.concat("yu"));
+    }
+
+    @Test
+    public void testReplace(){
+        String s ="121212121212121212";
+        System.out.println(s.replace('2','3'));
+
+    }
+
+    @Test
+    public void testsplit(){
+        String s ="hello word wew wre";
+        String[] strings = s.split("#");
+        System.out.println(strings);
+    }
+
+    @Test
+    public void testw(){
+        char ch = '0';
+        String s = "\\#";
+        System.out.println((((ch = s.charAt(1))-'0')|('9'-ch)));
+        System.out.println((((ch = s.charAt(1))-'A')|('Z'-ch)));
+        System.out.println((((ch = s.charAt(1))-'a')|('z'-ch)));
+        System.out.println((s.length() == 2 &&
+                s.charAt(0) == '\\' &&
+                (((ch = s.charAt(1))-'0')|('9'-ch)) < 0 &&
+                ((ch-'a')|('z'-ch)) < 0 &&
+                ((ch-'A')|('Z'-ch)) < 0));
+    }
+
+    @Test
+    public void testSplit(){
+        String s = "wewewewwwaw";
+        String[] strings = s.split("w");
+        System.out.println(strings);
+    }
+    @Test
+    public void testTrim(){
+        String s = "   234234234 ";
+        System.out.println(s.trim());
+    }
+    @Test
+    public void testTrim2(){
+
     }
 }
