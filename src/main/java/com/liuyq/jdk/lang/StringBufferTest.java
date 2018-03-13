ed98883d4070e06d647a2aee3ec81d5845e27f86
liuyq913 <2273852279@qq.com>
2018-03-13 21:55:13 +0800

    String源码

---
+++ src/main/java/com/liuyq/jdk/lang/StringBufferTest.java
@@ -0,0 +1,13 @@
+package com.liuyq.jdk.lang;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/3/12 0012.
+ */
+public class StringBufferTest {
+    @Test
+    public void test1(){
+        StringBuffer stringBuffer = new StringBuffer();
+    }
+}
