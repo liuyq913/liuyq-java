502baa719fea0073954ea7f09d02a4f0be417dcb
liuyqq <liuyq@cheok.com>
2018-04-03 15:24:42 +0800

    Merge branch 'master' of https://github.com/liuyq913/liuyq-java
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/liuyq/jdk/util/A.java
+++ src/main/java/com/liuyq/jdk/util/A.java
@@ -4,4 +4,13 @@ package com.liuyq.jdk.util;
  * Created by liuyq on 2018/4/2.
  */
 public class A {
+    public String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
