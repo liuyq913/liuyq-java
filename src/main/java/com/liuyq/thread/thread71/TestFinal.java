b25fe9b6abbbdd9af393f4044766707a68db197f
liuyq913 <2273852279@qq.com>
2018-08-06 23:06:40 +0800

    Merge branch 'master' of https://github.com/liuyq913/liuyq-java
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/main/java/com/liuyq/thread/thread71/TestFinal.java
@@ -0,0 +1,17 @@
+package com.liuyq.thread.thread71;
+
+/**
+ * Created by Administrator on 2018/7/23 0023.
+ */
+public class TestFinal {
+    private final int i;
+
+    TestFinal(Integer i){
+        this.i = i;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+}
