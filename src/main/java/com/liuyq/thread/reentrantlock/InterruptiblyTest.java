6f3ed215a451a0b245bce9422b4bae0469600df1
liuyq913 <2273852279@qq.com>
2018-11-05 22:59:44 +0800

    重入锁的两种用法

---
+++ src/main/java/com/liuyq/thread/reentrantlock/InterruptiblyTest.java
@@ -0,0 +1,52 @@
+package com.liuyq.thread.reentrantlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ReentrantLock中断方式获取锁
+ */
+public class InterruptiblyTest implements Runnable{
+    public static ReentrantLock lock1 = new ReentrantLock();
+    public static ReentrantLock lock2 = new ReentrantLock();
+    int lock;
+
+    public InterruptiblyTest(int lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (lock == 1) {
+                lock1.lockInterruptibly();  // 以可以响应中断的方式加锁
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}
+                lock2.lockInterruptibly();
+            } else {
+                lock2.lockInterruptibly();  // 以可以响应中断的方式加锁
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}
+                lock1.lockInterruptibly();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (lock1.isHeldByCurrentThread()) lock1.unlock();  // 注意判断方式
+            if (lock2.isHeldByCurrentThread()) lock2.unlock();
+            System.err.println(Thread.currentThread().getId() + "退出！");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        InterruptiblyTest deadLock1 = new InterruptiblyTest(1);
+        InterruptiblyTest deadLock2 = new InterruptiblyTest(2);
+        Thread t1 = new Thread(deadLock1);
+        Thread t2 = new Thread(deadLock2);
+        t1.start();t2.start();
+        Thread.sleep(1000);
+        t2.interrupt(); //中断，线程2会相应中断，并释放锁
+    }
+
+}
---
+++ src/main/java/com/liuyq/thread/reentrantlock/TryLockTest.java
@@ -0,0 +1,32 @@
+package com.liuyq.thread.reentrantlock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 限时获取锁，失败返回false
+ */
+public class TryLockTest implements Runnable{
+    public static ReentrantLock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        try {
+            if (lock.tryLock(1, TimeUnit.SECONDS)) { // 等待1秒
+                Thread.sleep(2000);  //休眠2秒
+            } else {
+                System.err.println(Thread.currentThread().getName() + "获取锁失败！");
+            }
+        } catch (Exception e) {
+            if (lock.isHeldByCurrentThread()) lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TryLockTest test = new TryLockTest();
+        Thread t1 = new Thread(test); t1.setName("线程1");
+        Thread t2 = new Thread(test); t1.setName("线程2");
+        t1.start();t2.start();
+    }
+
+}
